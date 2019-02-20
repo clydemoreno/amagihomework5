5a602d59195e6559c0cea9174f91319a89901824
clydemoreno <clydemoreno@mac.com>
2019-02-20 17:14:50 +0800

    3rd commit

--- src/KeyBoardEvents.js
+++ src/KeyBoardEvents.js
@@ -4,14 +4,28 @@ import React from 'react';
 
 class KeyBoardEvent extends React.Component{
     state = {displayText: ''};
+    handleEnter = (e) => {
+        // alert(e.key);
+        if(e.key === 'Enter'){
+            alert('You have entered: ' + this.state.displayText);
+        }
+        else if (e.key === 'Escape'){
+            // alert('Esc key hit');
+            this.setState({displayText:''}); 
+
+        }
+        e.preventDefault();
+    }
     handleOnKeyUp = (e) => {
         this.setState({displayText:e.target.value}); 
         // this.props.handleOnKeyUp(e);
+        this.handleEnter(e);
+        e.preventDefault();
     }
     render(){
         
         return(
-            <div>test
+            <div>Type anything in the textbox.  You can also type enter for some action.
 
                 <p >
                     <span>I'm in a local component</span>
